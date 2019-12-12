4247662c8a503fbce474ffa259d21d62e6172030
Mikej81 <stumpj@gmail.com>
2019-12-12 16:28:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# f5-scca-terraform
+ 
---
+++ provider.tf
@@ -0,0 +1,4 @@
+# Configure the Microsoft Azure Provider, replace Service Principal and Subscription with your own
+provider "azurerm" {
+  version = "=1.38.0"
+}
