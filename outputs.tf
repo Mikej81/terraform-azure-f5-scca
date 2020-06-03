1f50529baa551a79db4c5b319dbd538d97deb9ce
Mikej81 <stumpj@gmail.com>
2020-06-03 09:51:13 -0400

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,24 @@
 ## OUTPUTS ###
-# data "azurerm_public_ip" "lbpip" {
-#   name                = "${azurerm_public_ip.lbpip.name}"
-#   resource_group_name = "${azurerm_resource_group.main.name}"
-#   depends_on          = ["module.firewall.azurerm_virtual_machine_extension.f5vm02-run-startup-cmd"]
-# }
+data "azurerm_public_ip" "lbpip" {
+  name                = azurerm_public_ip.lbpip.name
+  resource_group_name = azurerm_resource_group.main.name
+}
 
-output "sg_id" { value = "${azurerm_network_security_group.main.id}" }
-output "sg_name" { value = "${azurerm_network_security_group.main.name}" }
+output sg_id { value = azurerm_network_security_group.main.id }
+output sg_name { value = azurerm_network_security_group.main.name }
 
-# output "ALB_app1_pip" { value = "${data.azurerm_public_ip.lbpip.ip_address}" }
-output "ALB_app1_pip" { value = "https://${azurerm_public_ip.lbpip.ip_address}" }
+# output ALB_app1_pip { value = data.azurerm_public_ip.lbpip.ip_address }
+output ALB_app1_pip { value = "https://${data.azurerm_public_ip.lbpip.ip_address}" }
 
 # single tier
-output "f5vm01_id" { value = "${module.firewall.f5vm01_id}"  }
-output "f5vm01_mgmt_private_ip" { value = "${module.firewall.f5vm01_mgmt_private_ip}" }
-output "f5vm01_mgmt_public_ip" { value = "https://${module.firewall.f5vm01_mgmt_public_ip}" }
-output "f5vm01_ext_private_ip" { value = "${module.firewall.f5vm01_ext_private_ip}" }
+output f5vm01_id { value = module.firewall.f5vm01_id  }
+output f5vm01_mgmt_private_ip { value = module.firewall.f5vm01_mgmt_private_ip }
+output f5vm01_mgmt_public_ip { value = "https://${module.firewall.f5vm01_mgmt_public_ip}" }
+output f5vm01_ext_private_ip { value = module.firewall.f5vm01_ext_private_ip }
 
-output "f5vm02_id" { value = "${module.firewall.f5vm02_id}"  }
-output "f5vm02_mgmt_private_ip" { value = "${module.firewall.f5vm02_mgmt_private_ip}" }
-output "f5vm02_mgmt_public_ip" { value = "https://${module.firewall.f5vm02_mgmt_public_ip}" }
-output "f5vm02_ext_private_ip" { value = "${module.firewall.f5vm02_ext_private_ip}" }
+output f5vm02_id { value = module.firewall.f5vm02_id  }
+output f5vm02_mgmt_private_ip { value = module.firewall.f5vm02_mgmt_private_ip }
+output f5vm02_mgmt_public_ip { value = "https://${module.firewall.f5vm02_mgmt_public_ip}" }
+output f5vm02_ext_private_ip { value = module.firewall.f5vm02_ext_private_ip }
 
 # three tier
